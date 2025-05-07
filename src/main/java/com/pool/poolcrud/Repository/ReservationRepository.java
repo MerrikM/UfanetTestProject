be9f266121b6661a18e6c6d6e53698e45ff67cff
MerrikM <merrikm.pl03@gmail.com>
2025-05-07 21:07:40 +0500

    Update ReservationRepository.java

--- src/main/java/com/pool/poolcrud/Repository/ReservationRepository.java
+++ src/main/java/com/pool/poolcrud/Repository/ReservationRepository.java
@@ -4,6 +4,13 @@ import com.pool.poolcrud.Model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    boolean existsByOrderId(String orderId);
+
+    Reservation findByOrderId(String orderId);
+
 }
