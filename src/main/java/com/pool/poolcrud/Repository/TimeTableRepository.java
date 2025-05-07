8f51c9fde0aeb03f6092e6e07bcbb8c81e6ac048
MerrikM <merrikm.pl03@gmail.com>
2025-05-07 21:07:45 +0500

    Update TimeTableRepository.java

--- src/main/java/com/pool/poolcrud/Repository/TimeTableRepository.java
+++ src/main/java/com/pool/poolcrud/Repository/TimeTableRepository.java
@@ -1,10 +1,12 @@
 package com.pool.poolcrud.Repository;
 
+import com.pool.poolcrud.Model.Pool;
 import com.pool.poolcrud.Model.TimeTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
@@ -13,4 +15,5 @@ public interface TimeTableRepository extends JpaRepository<TimeTable, Long> {
     List<TimeTable> findByPoolIdAndCurrentBookingsLessThan(Long poolId, int maxBookings);
 
     List<TimeTable> findByPoolIdAndDate(Long poolId, LocalDate date);
+    TimeTable findByPoolAndDateAndTime(Pool pool, LocalDate date, LocalTime time);
 }
