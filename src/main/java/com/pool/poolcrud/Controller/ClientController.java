28ad6e638d774bb85647aee77269802cc1cfe1fd
MerrikM <merrikm.pl03@gmail.com>
2025-05-08 12:00:58 +0500

    Update ClientController.java

--- src/main/java/com/pool/poolcrud/Controller/ClientController.java
+++ src/main/java/com/pool/poolcrud/Controller/ClientController.java
@@ -1,6 +1,7 @@
 package com.pool.poolcrud.Controller;
 
 import com.pool.poolcrud.Model.Client;
+import com.pool.poolcrud.Model.Pool;
 import com.pool.poolcrud.Model.Reservation;
 import com.pool.poolcrud.Service.ClientService;
 import org.apache.coyote.Response;
@@ -32,8 +33,18 @@ public class ClientController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<Client> createClient(@RequestBody Client client) {
-        return ResponseEntity.ok(clientService.createClient(client));
+    public ResponseEntity<Client> createClient(@RequestBody ClientCreateRequest client) {
+
+        Client newClient = new Client();
+        newClient.setName(client.getName());
+        newClient.setSurname(client.getSurname());
+        newClient.setPatronymic(client.getPatronymic());
+        newClient.setNumber(client.getNumber());
+        newClient.setEmail(client.getEmail());
+
+        Client createdClient = clientService.createClient(newClient);
+
+        return ResponseEntity.ok(createdClient);
     }
 
     @PostMapping("/{id}/update")
@@ -45,5 +56,53 @@ public class ClientController {
         }
 
         return ResponseEntity.ok(client);
+    }
+
+    public static class ClientCreateRequest {
+        private String name;
+        private String surname;
+        private String patronymic;
+        private String number;
+        private String email;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+
+        public String getPatronymic() {
+            return patronymic;
+        }
+
+        public void setPatronymic(String patronymic) {
+            this.patronymic = patronymic;
+        }
+
+        public String getNumber() {
+            return number;
+        }
+
+        public void setNumber(String number) {
+            this.number = number;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
     }
 }
