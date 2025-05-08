ef7c6ff0e933c38ada7da217ec3be52db3e7c976
MerrikM <merrikm.pl03@gmail.com>
2025-05-08 12:02:04 +0500

    Update ReservationController.java

--- src/main/java/com/pool/poolcrud/Controller/ReservationController.java
+++ src/main/java/com/pool/poolcrud/Controller/ReservationController.java
@@ -1,22 +1,27 @@
 package com.pool.poolcrud.Controller;
 
+import com.pool.poolcrud.Model.Pool;
 import com.pool.poolcrud.Model.Reservation;
+import com.pool.poolcrud.Service.PoolService;
 import com.pool.poolcrud.Service.ReservationService;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/api/v0/pool/timetable/")
 public class ReservationController {
 
     private final ReservationService reservationService;
+    private final PoolService poolService;
 
-    public ReservationController(ReservationService reservationService) {
+    public ReservationController(ReservationService reservationService, PoolService poolService) {
         this.reservationService = reservationService;
+        this.poolService = poolService;
     }
 
     @PostMapping("/{poolId}/reserve")
@@ -33,7 +38,7 @@ public class ReservationController {
 
     @GetMapping("/{poolId}/cancel")
     public ResponseEntity<String> cancel(@PathVariable("poolId") Long poolId,
-                                              @RequestBody CancelRequest cancelRequest) {
+                                         @RequestBody CancelRequest cancelRequest) {
         reservationService.cancel(
                 cancelRequest.getClientId(),
                 cancelRequest.getOrderId()
@@ -41,6 +46,21 @@ public class ReservationController {
 
         return ResponseEntity.ok("Запись успешно отменена");
     }
+
+    @GetMapping("/{poolId}/getByFIO")
+    public ResponseEntity<List<Reservation>> getReservationByFIO(@PathVariable("poolId") Long poolId,
+                                                                      @RequestBody ReservationByFIORequest reservationRequest) {
+        List<Reservation> reservationList = reservationService.getByFIO(
+                reservationRequest.getName(),
+                reservationRequest.getSurname(),
+                reservationRequest.getPatronymic()
+        );
+        // Pool pool = poolService.getPoolById(poolId);
+        return ResponseEntity.ok(reservationList);
+    }
+
+
+    //public ResponseEntity<List<Reservation>> getReservationByDate();
 
     public static class ReservationRequest {
         private Long clientId;
@@ -81,6 +101,36 @@ public class ReservationController {
 
         public void setOrderId(String orderId) {
             this.orderId = orderId;
+        }
+    }
+
+    public static class ReservationByFIORequest {
+        private String name;
+        private String surname;
+        private String patronymic;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+
+        public String getPatronymic() {
+            return patronymic;
+        }
+
+        public void setPatronymic(String patronymic) {
+            this.patronymic = patronymic;
         }
     }
 }
