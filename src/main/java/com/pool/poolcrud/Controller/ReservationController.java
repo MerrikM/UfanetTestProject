1dc9e76eacebb834d4987361ea849548385a2d32
MerrikM <merrikm.pl03@gmail.com>
2025-05-09 23:00:37 +0500

    Update ReservationController.java
    Удалены статические классы, которые теперь перенесены в пакет DTO
    
    Добавлены обработки исключений

--- src/main/java/com/pool/poolcrud/Controller/ReservationController.java
+++ src/main/java/com/pool/poolcrud/Controller/ReservationController.java
@@ -1,14 +1,12 @@
 package com.pool.poolcrud.Controller;
 
-import com.pool.poolcrud.Model.Pool;
+import com.pool.poolcrud.DTO.Reservation.*;
 import com.pool.poolcrud.Model.Reservation;
-import com.pool.poolcrud.Service.PoolService;
 import com.pool.poolcrud.Service.ReservationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -17,120 +15,91 @@ import java.util.Map;
 public class ReservationController {
 
     private final ReservationService reservationService;
-    private final PoolService poolService;
 
-    public ReservationController(ReservationService reservationService, PoolService poolService) {
+    public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
-        this.poolService = poolService;
     }
 
     @PostMapping("/{poolId}/reserve")
-    public ResponseEntity<Reservation> reserve(@PathVariable("poolId") Long poolId,
-                                               @RequestBody ReservationRequest reservationRequest) {
-        Reservation reservation = reservationService.reserve(
-                reservationRequest.getClientId(),
-                poolId,
-                reservationRequest.getDateTime()
-        );
+    public ResponseEntity<?> reserve(@PathVariable("poolId") Long poolId,
+                                     @RequestBody ReservationRequestDTO reservationRequestDTO) {
+        try {
+            Reservation reservation = reservationService.reserve(
+                    reservationRequestDTO.getClientId(),
+                    poolId,
+                    reservationRequestDTO.getDateTime()
+            );
+            return ResponseEntity.ok(reservation);
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
+    }
 
-        return ResponseEntity.ok(reservation);
+    @PostMapping("/{poolId}/reserve-interval")
+    public ResponseEntity<?> reserveInterval( @PathVariable("poolId") Long poolId,
+                                              @RequestBody ReservationIntervalRequest request) {
+        try {
+            List<Reservation> reservations = reservationService.reserveForAFewHours(
+                    request.getClientId(),
+                    poolId,
+                    request.getDate(),
+                    request.getStart(),
+                    request.getEnd()
+            );
+            return ResponseEntity.ok(reservations);
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
     }
 
     @GetMapping("/{poolId}/cancel")
-    public ResponseEntity<String> cancel(@PathVariable("poolId") Long poolId,
-                                         @RequestBody CancelRequest cancelRequest) {
-        reservationService.cancel(
-                cancelRequest.getClientId(),
-                cancelRequest.getOrderId()
+    public ResponseEntity<?> cancel(@PathVariable("poolId") Long poolId,
+                                    @RequestBody CancelReservationDTO reservationDTO) {
+        try {
+            reservationService.cancel(
+                    reservationDTO.getClientId(),
+                    reservationDTO.getOrderId()
+            );
+            return ResponseEntity.ok("Запись успешно отменена");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
+    }
+
+    @PostMapping("/{poolId}/getByFIO")
+    public ResponseEntity<?> getReservationByFIO(@PathVariable("poolId") Long poolId,
+                                                 @RequestBody ReservationByFIODTO reservationDTO) {
+        try {
+            List<Reservation> reservationList = reservationService.getByFIO(
+                    reservationDTO.getName(),
+                    reservationDTO.getSurname(),
+                    reservationDTO.getPatronymic()
+            );
+
+            return ResponseEntity.ok(reservationList);
+        } catch (IllegalStateException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
+    }
+
+    @PostMapping("/{poolId}/getByDate")
+    public ResponseEntity<List<Reservation>> getReservationByDate(@PathVariable("poolId") Long poolId,
+                                                                  @RequestBody ReservationRequestByDate reservationDTO) {
+        List<Reservation> reservations = reservationService.getByReservationDay(
+                reservationDTO.getClientId(),
+                reservationDTO.getReservationDate()
         );
-
-        return ResponseEntity.ok("Запись успешно отменена");
+        return ResponseEntity.ok(reservations);
     }
 
-    @GetMapping("/{poolId}/getByFIO")
-    public ResponseEntity<List<Reservation>> getReservationByFIO(@PathVariable("poolId") Long poolId,
-                                                                      @RequestBody ReservationByFIORequest reservationRequest) {
-        List<Reservation> reservationList = reservationService.getByFIO(
-                reservationRequest.getName(),
-                reservationRequest.getSurname(),
-                reservationRequest.getPatronymic()
-        );
-        // Pool pool = poolService.getPoolById(poolId);
-        return ResponseEntity.ok(reservationList);
-    }
-
-
-    //public ResponseEntity<List<Reservation>> getReservationByDate();
-
-    public static class ReservationRequest {
-        private Long clientId;
-        private LocalDateTime dateTime;
-
-        public Long getClientId() {
-            return clientId;
-        }
-
-        public void setClientId(Long clientId) {
-            this.clientId = clientId;
-        }
-
-        public LocalDateTime getDateTime() {
-            return dateTime;
-        }
-
-        public void setDateTime(LocalDateTime dateTime) {
-            this.dateTime = dateTime;
-        }
-    }
-
-    public static class CancelRequest {
-        private Long clientId;
-        private String orderId;
-
-        public Long getClientId() {
-            return clientId;
-        }
-
-        public void setClientId(Long clientId) {
-            this.clientId = clientId;
-        }
-
-        public String getOrderId() {
-            return orderId;
-        }
-
-        public void setOrderId(String orderId) {
-            this.orderId = orderId;
-        }
-    }
-
-    public static class ReservationByFIORequest {
-        private String name;
-        private String surname;
-        private String patronymic;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getSurname() {
-            return surname;
-        }
-
-        public void setSurname(String surname) {
-            this.surname = surname;
-        }
-
-        public String getPatronymic() {
-            return patronymic;
-        }
-
-        public void setPatronymic(String patronymic) {
-            this.patronymic = patronymic;
+    @PostMapping("/{poolId}/confirm")
+    public ResponseEntity<?> confirmVisit(@PathVariable("poolId") Long poolId,
+                                          @RequestBody ConfirmReservationDTO reservationDTO) {
+        try {
+            reservationService.confirmVisit(reservationDTO.getOrderId());
+            return ResponseEntity.ok("Посещение успешно подтверждено");
+        } catch (IllegalStateException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
         }
     }
 }
