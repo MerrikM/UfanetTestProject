b06599db055a02fe4df7fdeb2af826a7d4becd80
MerrikM <merrikm.pl03@gmail.com>
2025-05-10 00:03:40 +0500

    Update ClientController.java
    Удалены статические классы и перемещены в пакет DTO
    
    Добавлены обработки исключений

--- src/main/java/com/pool/poolcrud/Controller/ClientController.java
+++ src/main/java/com/pool/poolcrud/Controller/ClientController.java
@@ -1,14 +1,14 @@
 package com.pool.poolcrud.Controller;
 
+import com.pool.poolcrud.DTO.ClientDTO;
 import com.pool.poolcrud.Model.Client;
-import com.pool.poolcrud.Model.Pool;
-import com.pool.poolcrud.Model.Reservation;
 import com.pool.poolcrud.Service.ClientService;
-import org.apache.coyote.Response;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v0/pool/client/")
@@ -21,9 +21,13 @@ public class ClientController {
     }
 
     @GetMapping("/{id}/get")
-    public ResponseEntity<Client> getClient(@PathVariable("id") Long id) {
-        Client client = clientService.getById(id);
-        return ResponseEntity.ok(client);
+    public ResponseEntity<?> getClient(@PathVariable("id") Long id) {
+        try {
+            Client client = clientService.getById(id);
+            return ResponseEntity.ok(client);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
     }
 
     @GetMapping("/all")
@@ -33,76 +37,27 @@ public class ClientController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<Client> createClient(@RequestBody ClientCreateRequest client) {
-
-        Client newClient = new Client();
-        newClient.setName(client.getName());
-        newClient.setSurname(client.getSurname());
-        newClient.setPatronymic(client.getPatronymic());
-        newClient.setNumber(client.getNumber());
-        newClient.setEmail(client.getEmail());
-
-        Client createdClient = clientService.createClient(newClient);
-
-        return ResponseEntity.ok(createdClient);
+    public ResponseEntity<Client> createClient(@RequestBody ClientDTO client) {
+        return ResponseEntity.ok(clientService.createClient(client));
     }
 
     @PostMapping("/{id}/update")
-    public ResponseEntity<Client> updateClient(@PathVariable("id") Long id, @RequestBody Client client) {
-        Client updatedClient = clientService.updateClient(id, client);
+    public ResponseEntity<?> updateClient(@PathVariable("id") Long id, @RequestBody ClientDTO client) {
+        try {
+            Client updatedClient = clientService.updateClient(id, client);
 
-        if (updatedClient == null) {
-            return ResponseEntity.notFound().build();
+            if (updatedClient == null) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(updatedClient);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
         }
-
-        return ResponseEntity.ok(client);
     }
 
-    public static class ClientCreateRequest {
-        private String name;
-        private String surname;
-        private String patronymic;
-        private String number;
-        private String email;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getSurname() {
-            return surname;
-        }
-
-        public void setSurname(String surname) {
-            this.surname = surname;
-        }
-
-        public String getPatronymic() {
-            return patronymic;
-        }
-
-        public void setPatronymic(String patronymic) {
-            this.patronymic = patronymic;
-        }
-
-        public String getNumber() {
-            return number;
-        }
-
-        public void setNumber(String number) {
-            this.number = number;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<?> deleteClient(@PathVariable Long id) {
+        clientService.deleteClient(id);
+        return ResponseEntity.ok().build();
     }
 }
